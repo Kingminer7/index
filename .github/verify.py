8d6b8d2171ee91888958032b6013ab90c1fadf06
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-07-31 09:51:59 -0400

    Create verify.py

---
+++ .github/verify.py
@@ -0,0 +1,10 @@
+import sys
+import json
+from pathlib import Path
+
+# index_path = Path(sys.argv[1])
+issue_author = sys.argv[2]
+
+# config = json.load(open(index_path / "config.json", "r"))
+config = json.load(open("config.json", "r"))
+print("YES" if issue_author in config["verified"] else "NO")
